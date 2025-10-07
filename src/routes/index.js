ba5a80b256e0a9da8fcb74b482086e693ce72919
Kpivarias <kennayvansan@gmail.com>
2025-10-06 22:16:04 -0300

    modified:   src/routes/index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,17 +1,49 @@
+import express from 'express';
+import * as animalController from '../controllers/animalController.js';
+import * as UsuarioController from '../controllers/usuarioController.js';
+import * as QuestionarioController from '../controllers/questionarioController.js';
+import * as PedidoAdocaoController from '../controllers/pedidoAdocaoController.js';
+import * as DoacaoController from '../controllers/doacaoController.js';
+const adminRouter = express.Router();
+// Rotas admin para animais
+adminRouter.get('/animais', animalController.getAll);
+adminRouter.patch('/animais/:id', animalController.update);
+adminRouter.delete('/animais/:id', animalController.remove);
+
+// Rotas PATCH para animais e tutores
+router.patch('/animais/:id', animalController.update);
+router.patch('/tutores/:id', UsuarioController.update);
+
+// Aliases para recursos
+router.post('/tutores', UsuarioController.create);
+router.get('/tutores/:id', UsuarioController.getById);
+router.post('/questionário', QuestionarioController.create);
+router.post('/adocoes', PedidoAdocaoController.create);
+router.post('/login', UsuarioController.login);
+router.post('/doacoes', DoacaoController.create);
+
+// Adiciona adminRouter sob /admin
+router.use('/admin', adminRouter);
 import { Router } from 'express';
 
-import animalRoutes from './animalRoutes.js';
-import usuarioRoutes from './usuarioRoutes.js';
-import questionarioRoutes from './questionarioRoutes.js';
-import pedidoAdocaoRoutes from './pedidoAdocaoRoutes.js';
-import doacaoRoutes from './doacaoRoutes.js';
 
 const router = Router();
+// Rotas públicas
+router.post('/animais', animalController.create);
+router.get('/animais', animalController.getAll);
+router.get('/animais/:id', animalController.getById);
 
-router.use('/animais', animalRoutes);
-router.use('/usuarios', usuarioRoutes);
-router.use('/questionarios', questionarioRoutes);
-router.use('/pedidos-adocao', pedidoAdocaoRoutes);
-router.use('/doacoes', doacaoRoutes);
+router.post('/tutores', UsuarioController.create);
+router.patch('/tutores/:id', UsuarioController.update);
+router.get('/tutores/:id', UsuarioController.getById);
+
+router.post('/questionário', QuestionarioController.create);
+router.post('/adocoes', PedidoAdocaoController.create);
+
+// Rotas admin
+// (declarado acima)
+
+router.post('/login', UsuarioController.login);
+router.post('/doacoes', DoacaoController.create);
 
 export default router;
